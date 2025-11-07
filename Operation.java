0f03e003c2f66debd5126a0276d19a3c5dd92cfa
XAESHKA <stepa150206.com@gmail.com>
2025-11-07 20:29:17 +0700

    ченибудь

--- Operation.java
+++ Operation.java
@@ -3,28 +3,35 @@ import java.util.Date;
 
 public class Operation {
     private Date date;
-    private String type;
+    private OperationType type;
     private double sum;
     private String info;
     
-    public Operation(String type, double sum, String info) {
+    public Operation(OperationType type, double sum) {
         this.date = new Date();
         this.type = type;
         this.sum = sum;
-        this.info = info;
+        
+        if (type == OperationType.DEPOSIT) {
+            this.info = "Пополнение счета через банкомат";
+        } else if (type == OperationType.WITHDRAWAL) {
+            this.info = "Снятие наличных в банкомате";
+        } else {
+            this.info = "Пополнение телефона";
+        }
     }
     
     public Date getDate() {
-    return date; 
-}
-
-    public String getType() { 
-        return type; 
-    
+        return date; 
     }
+
+    public OperationType getType() { 
+        return type; 
+    }
+    
     public double getSum() {
-         return sum; 
-        }
+        return sum; 
+    }
 
     public String getInfo() { 
         return info; 
@@ -36,4 +43,4 @@ public class Operation {
         String formattedDate = dateFormat.format(date);
         return "Дата: " + formattedDate + " | " + type + " | " + sum + " руб. | " + info;
     }
-}// чоко и пай
+}
