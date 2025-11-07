72f9e1bf4511db46391e920bc9cc3bf8ad7f4651
root <root@DESKTOP-7UMJMF6.localdomain>
2025-11-07 19:51:59 +0700

    Operation

---
+++ Operation.java
@@ -0,0 +1,39 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Operation {
+    private Date date;
+    private String type;
+    private double sum;
+    private String info;
+    
+    public Operation(String type, double sum, String info) {
+        this.date = new Date();
+        this.type = type;
+        this.sum = sum;
+        this.info = info;
+    }
+    
+    public Date getDate() {
+    return date; 
+}
+
+    public String getType() { 
+        return type; 
+    
+    }
+    public double getSum() {
+         return sum; 
+        }
+
+    public String getInfo() { 
+        return info; 
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        String formattedDate = dateFormat.format(date);
+        return "Дата: " + formattedDate + " | " + type + " | " + sum + " руб. | " + info;
+    }
+}
