83a6ea99f27a8c1db0a4d8aba8063f36fa4eb44f
prohorJR <egor.parkhomenko.01@mail.ru>
2025-11-08 01:48:48 +0700

    Account

---
+++ Account.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+    private String id;
+    private String pin;
+    private double balance;
+    private List<Operation> operations;
+    
+    public Account(String id, String pin, double firstBalance) {
+        this.id = id;
+        this.pin = pin;
+        this.balance = firstBalance;
+        this.operations = new ArrayList<Operation>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean checkPin(String inputPin) { 
+        if (inputPin == null) {
+            return false;
+        }
+        return this.pin.equals(inputPin);
+    }
+
+    public double getBalance() { 
+        return balance;
+    }
+
+    public void deposit(double sum) { 
+        if (sum > 0) { 
+            balance += sum;
+            operations.add(new Operation(OperationType.DEPOSIT, sum));
+        }
+    }
+
+    public boolean withdraw(double sum) {
+        if (sum > 0 && balance >= sum) {
+            balance -= sum;
+            operations.add(new Operation(OperationType.WITHDRAWAL, sum));
+            return true;
+        }
+        return false;
+    }
+
+    public List<Operation> getOperations() {
+        return new ArrayList<Operation>(operations);
+    }
+}
